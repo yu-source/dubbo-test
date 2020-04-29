a040fbec744de4f6b5ecd50dd86ced022c703ccc
刘坚 <jian.liu@meinergy.cn>
2020-04-29 10:32:01 +0800

    第一次测试提交

---
+++ dubbo-demo/src/test/java/cn/meinergy/dubbo/demo/DubboDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.meinergy.dubbo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
